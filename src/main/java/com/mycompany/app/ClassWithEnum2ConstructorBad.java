09df82bbdb764fd7549e29c4535781d19c213ff0
Franck Arnulfo <franck.arnulfo@gmail.com>
2022-07-11 23:01:34 +0200

    Create ClassWithEnum2ConstructorBad.java

---
+++ src/main/java/com/mycompany/app/ClassWithEnum2ConstructorBad.java
@@ -0,0 +1,14 @@
+public class ClassWithEnum2ConstructorBad {
+
+    public enum Enum2 {
+        EnumVal1,
+        EnumVal2
+    }
+
+    public void foo(Enum2 enum2) {
+        switch (enum2) {
+            case EnumVal1:
+                break;
+        }
+    }
+}
